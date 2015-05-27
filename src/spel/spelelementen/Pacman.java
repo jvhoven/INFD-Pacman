c5adac3fba81b7a6bc8e3e684efb13665b6bd08b
Hans <Hans@Spider>
2015-05-27 20:44:31 +0200

    PLaatjes worden nu getekend voor pacman en spookjes

--- src/spel/spelelementen/Pacman.java
+++ src/spel/spelelementen/Pacman.java
@@ -9,6 +9,11 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 import spel.levelelementen.LeegVakje;
 import spel.levelelementen.Positie;
 import spel.levelelementen.Vakje;
@@ -18,22 +23,38 @@ import spel.levelelementen.Vakje;
  * @author Hans
  */
 public class Pacman extends Poppetje implements KeyListener {
-    
+
     private boolean arrowKeyPressed = false;
-    
+
+    private BufferedImage pacmanPlaatje = null;
+
     public Pacman(LeegVakje startVakje) {
         this.huidigVakje = startVakje;
+
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            URL urlPath = classLoader.getResource("images/pacman.png");
+            if (urlPath != null) {
+                File file = new File(urlPath.getFile());
+                pacmanPlaatje = ImageIO.read(file);
+            }
+        } catch (IOException e) {
+            System.err.print(e);
+        }
     }
 
     @Override
     public void teken(Graphics2D g) {
-        g.setColor(Color.yellow);
 
         if (huidigVakje != null) {
             int x = huidigVakje.positie.x * Vakje.SIZE - Vakje.SIZE + 5;
             int y = huidigVakje.positie.y * Vakje.SIZE - Vakje.SIZE + 5;
-
-            g.fillOval(x, y, Vakje.SIZE - 10, Vakje.SIZE - 10);
+            if (pacmanPlaatje == null) {
+                g.setColor(Color.yellow);
+                g.fillOval(x, y, Vakje.SIZE - 10, Vakje.SIZE - 10);
+            } else {
+                g.drawImage(pacmanPlaatje, x, y, null);
+            }
         }
     }
 
@@ -42,44 +63,44 @@ public class Pacman extends Poppetje implements KeyListener {
 
     }
 
-    private void beweeg(Positie buurPositie) {
-        Vakje buurVakje = this.huidigVakje.getBuurVakje(buurPositie);
+    private void controleerBuurvakje(Positie nieuwePositie) {
+        Vakje buurVakje = this.huidigVakje.getBuurVakje(nieuwePositie);
 
         if (buurVakje != null && buurVakje instanceof LeegVakje) {
             this.beweegNaar((LeegVakje) buurVakje);
         }
     }
-    
-    private Positie getPositie(KeyEvent e) {
-        
-        Positie buurPositie = null;
+
+    private Positie getNieuwePositie(KeyEvent e) {
+
+        Positie nieuwePositie = null;
         Positie huidigePositie = this.huidigVakje.positie;
-        
+
         switch (e.getKeyCode()) {
             case KeyEvent.VK_DOWN:
-                buurPositie = new Positie(huidigePositie.x, huidigePositie.y + 1);
+                nieuwePositie = new Positie(huidigePositie.x, huidigePositie.y + 1);
                 break;
             case KeyEvent.VK_UP:
-                buurPositie = new Positie(huidigePositie.x, huidigePositie.y - 1);
+                nieuwePositie = new Positie(huidigePositie.x, huidigePositie.y - 1);
                 break;
             case KeyEvent.VK_LEFT:
-                buurPositie = new Positie(huidigePositie.x - 1, huidigePositie.y);
+                nieuwePositie = new Positie(huidigePositie.x - 1, huidigePositie.y);
                 break;
             case KeyEvent.VK_RIGHT:
-                buurPositie = new Positie(huidigePositie.x + 1, huidigePositie.y);
+                nieuwePositie = new Positie(huidigePositie.x + 1, huidigePositie.y);
                 break;
         }
-        
-        return buurPositie;
+
+        return nieuwePositie;
     }
-    
+
     @Override
     public void keyPressed(KeyEvent e) {
         if (!arrowKeyPressed) {
-            Positie buurPositie = getPositie(e);
-           
+            Positie buurPositie = getNieuwePositie(e);
+
             if (buurPositie != null) {
-                this.beweeg(buurPositie);
+                this.controleerBuurvakje(buurPositie);
             }
         }
         arrowKeyPressed = true;
--- src/spel/spelelementen/Spookje.java
+++ src/spel/spelelementen/Spookje.java
@@ -7,6 +7,11 @@ package spel.spelelementen;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 import spel.levelelementen.LeegVakje;
 import spel.levelelementen.Vakje;
 
@@ -15,21 +20,37 @@ import spel.levelelementen.Vakje;
  * @author Hans
  */
 public class Spookje extends Poppetje {
-    
+
+    private BufferedImage spookPlaatje = null;
+
     public Spookje(Vakje startVakje) {
-        setHuidigVakje((LeegVakje)startVakje);
+        setHuidigVakje((LeegVakje) startVakje);
+
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            URL urlPath = classLoader.getResource("images/spook.png");
+            if (urlPath != null) {
+                File file = new File(urlPath.getFile());
+                spookPlaatje = ImageIO.read(file);
+            }
+        } catch (IOException e) {
+            System.err.print(e);
+        }
     }
-    
+
     @Override
     public void teken(Graphics2D g) {
-        g.setColor(Color.blue);
 
         if (huidigVakje != null) {
             int x = huidigVakje.positie.x * Vakje.SIZE - Vakje.SIZE + 5;
             int y = huidigVakje.positie.y * Vakje.SIZE - Vakje.SIZE + 5;
-
-            g.fillOval(x, y, Vakje.SIZE - 10, Vakje.SIZE - 10);
+            if (spookPlaatje == null) {
+                g.setColor(Color.blue);
+                g.fillOval(x, y, Vakje.SIZE - 10, Vakje.SIZE - 10);
+            } else {
+                g.drawImage(spookPlaatje, x, y, null);
+            }
         }
     }
-    
+
 }
